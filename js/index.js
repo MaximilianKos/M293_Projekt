4f1b7678f20865fa92a53d8f8b77cc886029a2bd
MaximilianKos <maximiliankos.x@gmail.com>
2023-04-21 10:06:57 +0200

    own about page, added navbar, misc changes

---
+++ js/index.js
@@ -0,0 +1,36 @@
+(function () {
+	'use strict';
+	const typed = document.querySelector('.typed');
+	if (typed) {
+		let typed_strings = typed.getAttribute('data-typed-items').split(',');
+		new Typed('.typed', {
+			strings: typed_strings,
+			loop: true,
+			typeSpeed: 100,
+			backSpeed: 50,
+			backDelay: 2000,
+		});
+	}
+})();
+
+let selectedLink = document.querySelector('.selectedLink');
+
+function checkActive() {
+	document.querySelectorAll('nav li').forEach((item) => {
+		if (item.dataset.active == 'true') {
+			selectedLink.style.transform = `translateX(${item.offsetLeft + item.offsetWidth / 2 - 24 + 'px'})`;
+		}
+	});
+}
+
+document.querySelectorAll('nav li').forEach((item) => {
+	item.addEventListener('click', (e) => {
+		document.querySelectorAll('nav li').forEach((i) => {
+			i.dataset.active = 'false';
+		});
+		e.currentTarget.dataset.active = 'true';
+		checkActive();
+	});
+});
+
+checkActive();
