a707d61d4843dae917473b7b0026ccbdc0ea732b
MaximilianKos <maximiliankos.x@gmail.com>
2023-04-24 08:54:21 +0200

    simple layout projects page

--- js/index.js
+++ js/index.js
@@ -12,25 +12,3 @@
 		});
 	}
 })();
-
-let selectedLink = document.querySelector('.selectedLink');
-
-function checkActive() {
-	document.querySelectorAll('nav li').forEach((item) => {
-		if (item.dataset.active == 'true') {
-			selectedLink.style.transform = `translateX(${item.offsetLeft + item.offsetWidth / 2 - 24 + 'px'})`;
-		}
-	});
-}
-
-document.querySelectorAll('nav li').forEach((item) => {
-	item.addEventListener('click', (e) => {
-		document.querySelectorAll('nav li').forEach((i) => {
-			i.dataset.active = 'false';
-		});
-		e.currentTarget.dataset.active = 'true';
-		checkActive();
-	});
-});
-
-checkActive();
